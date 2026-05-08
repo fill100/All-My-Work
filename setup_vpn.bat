3aae468e9dc90fc52bca583fce96041efb343477
fill100 <78307583+fill100@users.noreply.github.com>
2026-05-08 21:27:13 +0700

    Update setup_vpn.bat

--- setup_vpn.bat
+++ setup_vpn.bat
@@ -1,11 +1,11 @@
 @echo off
+chcp 65001 >nul
 :: 1. ตรวจสอบสิทธิ์ Admin
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (
-    goto UACPrompt
-) else ( goto gotAdmin )
+if '%errorlevel%' NEQ '0' ( goto UACPrompt ) else ( goto gotAdmin )
 
 :UACPrompt
+    echo Requesting Admin...
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
     "%temp%\getadmin.vbs"
@@ -16,36 +16,31 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 
-:: 2. สร้างโฟลเดอร์ทำงาน
+:: 2. สร้างโฟลเดอร์เก็บข้อมูล
 if not exist "C:\JVFS-IT" mkdir "C:\JVFS-IT"
 
-:: 3. สร้างไฟล์ Config (.vpl) แบบ Force เขียนค่าเข้าไป
-:: ไฟล์นี้จะเป็นตัวตั้งชื่อ Profile และ IP ให้เองอัตโนมัติ
-set VPL_FILE="C:\JVFS-IT\JVFS_Config.vpl"
-echo [VPN_Profile] > %VPL_FILE%
-echo Type=SSL >> %VPL_FILE%
-echo Name=JVFS_VPN >> %VPL_FILE%
-echo Server=119.110.207.194 >> %VPL_FILE%
-echo Port=20443 >> %VPL_FILE%
-echo UserPrompt=1 >> %VPL_FILE%
+:: 3. สร้างไฟล์ Config พิเศษ (สไตล์ Fortinet)
+set CONF_FILE=C:\JVFS-IT\jvfs_setup.vpl
+echo [VPN_PROFILE]> %CONF_FILE%
+echo name=JVFS_VPN>> %CONF_FILE%
+echo server=119.110.207.194:20443>> %CONF_FILE%
+echo type=sslvpn>> %CONF_FILE%
 
-:: 4. สั่งให้ FortiClient อ่านไฟล์นี้ (เพื่อ Import ค่าเข้าโปรแกรม)
-start "" %VPL_FILE%
-timeout /t 2 >nul
+:: 4. สั่งให้ Windows เปิดไฟล์นี้ด้วย FortiClient
+:: วิธีนี้จะทำให้มันเด้งถาม "Do you want to import?" ซึ่งชัวร์ที่สุด
+start "" "%CONF_FILE%"
 
-:: 5. สร้างไฟล์สำหรับเปิดโปรแกรมครั้งต่อๆ ไป (ผ่าน Protocol)
-echo @echo off > "C:\JVFS-IT\connect_vpn.bat"
-echo start "" "C:\Program Files\Fortinet\FortiClient\FortiClient.exe" >> "C:\JVFS-IT\connect_vpn.bat"
-
-:: 6. ลงทะเบียน Protocol jvfs-connect://
-reg add "HKCR\jvfs-connect" /ve /t REG_SZ /d "URL:JVFS VPN Protocol" /f
+:: 5. ตั้งค่า Protocol ให้เรียกเปิดโปรแกรมครั้งต่อไป
+reg add "HKCR\jvfs-connect" /ve /t REG_SZ /d "URL:JVFS VPN" /f
 reg add "HKCR\jvfs-connect" /v "URL Protocol" /t REG_SZ /d "" /f
-reg add "HKCR\jvfs-connect\shell\open\command" /ve /t REG_SZ /d "\"C:\JVFS-IT\connect_vpn.bat\"" /f
+reg add "HKCR\jvfs-connect\shell\open\command" /ve /t REG_SZ /d "\"C:\Program Files\Fortinet\FortiClient\FortiClient.exe\"" /f
 
+cls
 echo ========================================
-echo [ ขั้นตอนสุดท้าย ]
-echo - จะมีหน้าจอ FortiClient เด้งขึ้นมาถามว่า "Do you want to import...?"
-echo - ให้ User ตอบ "Yes" (ตกลง) แค่ครั้งแรกครั้งเดียวครับ
-echo - หลังจากนั้น Profile จะถูกตั้งค่าถาวรเลย!
+echo        [ ตั้งค่าเสร็จเรียบร้อย ]
+echo ========================================
+echo 1. จะมีหน้าต่างเด้งถามเรื่อง "Import Profile" ให้กด YES
+echo 2. จากนั้นโปรแกรม FortiClient จะมีชื่อ JVFS_VPN ขึ้นมาให้ใช้
+echo 3. ครั้งต่อไป กดเรียกจากหน้าเว็บได้เลย
 echo ========================================
 pause
